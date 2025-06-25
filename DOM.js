208ed68a102246b929e2e9efa17dcacc6dbf13fa
bkusuma0209 <barigeakusuma@gmail.com>
2025-06-25 15:11:39 +0530

    HTML-BASICS

---
+++ DOM.js
@@ -0,0 +1,12 @@
+// DOM in JavaScript
+// DOM stands for Document Object Model. It is a programming interface for HTML and XML documents.
+document.title="DOM in JavaScript";
+document.getElementById("header").innerHTML="Helloo, World!!";
+document.getElementById("paragraph").style.color = "blue";
+document.getElementById("paragraph").style.fontSize = "30px";
+document.getElementById("header").style.color = "black";
+document.body.style.backgroundColor="lightblue";
+document.getElementsByClassName("DS")[0].style.color="Red";
+document.getElementsByClassName("DS")[1].style.color="Green";
+document.getElementsByClassName("DS")[2].style.color="violet";
+document.getElementsByClassName("DS")[3].style.color="Orange";
