b748f1509ad6dbadc8f57f410536a0a95fd6c7af
bkusuma0209 <barigeakusuma@gmail.com>
2025-06-26 12:34:56 +0530

    HTML-BASICS

--- DOM.js
+++ DOM.js
@@ -10,3 +10,32 @@ document.getElementsByClassName("DS")[0].style.color="Red";
 document.getElementsByClassName("DS")[1].style.color="Green";
 document.getElementsByClassName("DS")[2].style.color="violet";
 document.getElementsByClassName("DS")[3].style.color="Orange";
+let Kusuma=document.getElementsByClassName("DSA");
+Kusuma[0].style.color="Red";
+Kusuma[1].style.color="Green";
+kusuma[2].style.color="Yellow";
+kusuma[3].style.color="Orange";
+document.querySelector(".query")[0].style.color="Red";
+let depts = document.querySelectorAll(".EB");
+elememts[0].style.color="violet";
+elememts[1].style.color="Green";
+elememts[2].style.color="Yellow";
+
+function changeBackground() {
+    if(document.body.style.backgroundColor === "lightblue")
+    {
+        document.body.style.backgroundColor = "red";
+    } 
+    else{
+        document.body.style.backgroundColor = "green";
+    }
+    }
+function changetext(){
+    document.getElementById("clg").innerHTML="CMRCET", "MLRD";
+    document.getElementById("clg").style.backgroundColor= "coral";
+    document.getElementById("clg").style.backgroundColor= "white";
+    document.getElementById("clg").style.fontSize= "30px";
+    document.getElementById("clg").style.color= "blue";
+    
+    
+}
